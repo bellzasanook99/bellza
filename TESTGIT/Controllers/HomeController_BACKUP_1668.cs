631e5de5fac8d6568c8b311902298103a5a8a501
aeakalak <akekaluk32@gmail.com>
2022-03-14 00:20:27 +0700

    commit

---
+++ TESTGIT/Controllers/HomeController_BACKUP_1668.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TESTGIT.Models;
+
+namespace TESTGIT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           string xxx = "test";
+<<<<<<< HEAD
+            string xxx1 = "test";
+
+            string xxx2 = "test";
+            string xxx3 = "test";
+            string xxx8883 = "test";
+
+=======
+            string xxx3 = "test";
+            string xxx4 = "test";
+
+            string xxx5 = "test";
+>>>>>>> bellDev
+            return View();
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
